084fe47c2a6e52d8a35208b76fb7f6963348a0fe
Venkat A Suresh <54540744+VenkataSuresh7989@users.noreply.github.com>
2023-08-04 20:33:12 +0530

    REST Service IP updated

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
 
-var baseIP = "127.0.0.1";
-axios.defaults.baseURL = 'http://'+ baseIP + ':8000';
+// var baseIP = "127.0.0.1";
+// axios.defaults.baseURL = 'http://'+ baseIP + ':8000';
+axios.defaults.baseURL = "https://ecommerce-k4ny.onrender.com";
 
 Vue.use(Vuex)
 
